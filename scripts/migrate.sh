015d7f70a3b5d3f5337160e93e646d90c126cdb2
sebastian.mihnea <sebastian.mihnea@napta.io>
2023-06-27 01:04:12 +0200

    Adapt scripts for mac

--- scripts/migrate.sh
+++ scripts/migrate.sh
@@ -1,3 +1,11 @@
-#! /bin/bash
+#! /bin/sh
 
-../vendor/bin/sail artisan migrate
+os=$(uname -s)
+
+if [ "$os" = "Darwin" ]; then
+    echo "Distribution : macOS"
+    cd ..
+    php artisan migrate
+else
+    ../vendor/bin/sail artisan migrate
+fi
