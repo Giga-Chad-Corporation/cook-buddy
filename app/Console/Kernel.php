4ba3e83632f58e376396b6e7cf430dbabd532ed0
Anakaryna <78374876+Anakaryna@users.noreply.github.com>
2023-07-21 23:32:50 +0200

    Security header added

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,4 +24,9 @@ class Kernel extends ConsoleKernel
 
         require base_path('routes/console.php');
     }
+
+    protected $middleware = [
+        // ...
+        \App\Http\Middleware\SecurityHeadersMiddleware::class,
+    ];
 }
---
+++ app/Http/Middleware/SecurityHeadersMiddleware.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+
+class SecurityHeadersMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param \Closure $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $response = $next($request);
+
+        $response->header('Strict-Transport-Security', 'max-age=31536000; includeSubDomains');
+        $response->header('X-Frame-Options', 'SAMEORIGIN');
+        $response->header('X-Content-Type-Options', 'nosniff');
+        $response->header('Referrer-Policy', 'no-referrer');
+
+        return $response;
+    }
+}
