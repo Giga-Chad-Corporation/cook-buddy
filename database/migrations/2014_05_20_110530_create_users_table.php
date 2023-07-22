efcd25248108d168a2cd193cd1da20ee739b40e3
Towga <kamiltebn@gmail.com>
2023-07-22 19:34:29 +0200

    added MustVerifyEmail to user model and revert to datetime

--- database/migrations/2014_05_20_110530_create_users_table.php
+++ database/migrations/2014_05_20_110530_create_users_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->string('last_name');
             $table->string('first_name');
             $table->string('email')->unique();
-            $table->boolean('email_verified')->nullable();
+            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('phone')->nullable();
             $table->string('username')->unique();
