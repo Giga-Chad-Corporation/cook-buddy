3813b9c0e4f55a01cf826d96c30bf3ef08920e7b
Anakaryna <78374876+Anakaryna@users.noreply.github.com>
2023-07-23 00:53:33 +0200

    Provider table profile_complete added

--- database/migrations/2023_05_21_112149_create_providers_table.php
+++ database/migrations/2023_05_21_112149_create_providers_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('providers', function (Blueprint $table) {
             $table->id();
             $table->float('revenue')->default(0);
+            $table->boolean('is_profile_complete')->default(false);
             $table->timestamps();
         });
     }
